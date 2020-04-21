09d776b49e1b01e2122975fd0a6d4300f15bc0ae
Vitor <vitorantoniops@gmail.com>
2020-04-21 18:50:18 -0300

    adaptacao ao namespace vitor::txt::

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "lib_do_sucesso.h"
 #include "auxiliar_do_sucesso.h"
 using namespace std;
-
+using namespace vitor::txt;
 int main()
 {
     printOptions();
@@ -17,64 +17,64 @@ int main()
         switch(option)
         {
             case 1:
-                txt_Desenha_Retangulo();
+                vitor::txt::txt_Desenha_Retangulo();
                 break;
             case 2:
-                txt_Volume_Esfera();
+                vitor::txt::txt_Volume_Esfera();
                 break;
             case 3:
-                txt_Fat_x();
+                vitor::txt::txt_Fat_x();
                 break;
             case 4:
-                txt_Fib();
+                vitor::txt::txt_Fib();
                 break;
             case 5:
-                txt_Regular();
+                vitor::txt::txt_Regular();
                 break;
             case 6:
-                txt_Combinacoes();
+                vitor::txt::txt_Combinacoes();
                 break;
             case 7:
-                txt_Palindromo();
+                vitor::txt::txt_Palindromo();
                 break;
             case 8:
-                txt_To_Lower_Case();
+                vitor::txt::txt_To_Lower_Case();
                 break;
             case 9:
-                txt_To_Upper_Case();
+                vitor::txt::txt_To_Upper_Case();
                 break;
             case 10:
-                txt_Caixa_Com_Texto_Centralizado();
+                vitor::txt::txt_Caixa_Com_Texto_Centralizado();
                 break;
             case 11:
-                txt_Ordena_Tres();
+                vitor::txt::txt_Ordena_Tres();
                 break;
             case 12:
-                txt_Tamanho_String();
+                vitor::txt::txt_Tamanho_String();
                 break;
             case 13:
-                txt_Conta_String();
+                vitor::txt::txt_Conta_String();
                 break;
             case 14:
-                txt_String_To_Int();
+                vitor::txt::txt_String_To_Int();
                 break;
             case 15:
-                txt_No_Spaces();
+                vitor::txt::txt_No_Spaces();
                 break;
             case 16:
-                txt_Sub_Strings();
+                vitor::txt::txt_Sub_Strings();
                 break;
             case 17:
-                txt_Sub_Check();
+                vitor::txt::txt_Sub_Check();
                 break;
             case 18:
-                txt_Soma_Gigante();
+                vitor::txt::txt_Soma_Gigante();
                 break;
             case 19:
-                txt_find_Greater_and_Smaller();
+                vitor::txt::txt_find_Greater_and_Smaller();
                 break;
             case 20:
-                txt_Soma_Matriz();
+                vitor::txt::txt_Soma_Matriz();
                 break;
             default:
                 cout << "YOU HAD ONE JOB!";
